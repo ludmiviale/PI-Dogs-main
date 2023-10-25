a64c0f7ca37b48f227dfbf2e4ef65b604bbbd5a9
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-25 13:32:17 -0300

    updating dogsHandler

--- api/src/handlers/dogsHandler.js
+++ api/src/handlers/dogsHandler.js
@@ -1,38 +1,70 @@
+const {
+  getAllDogsFromApi,
+  getAllDogsFromDB,
+} = require("../controllers/dogsControllers/getAllDogs");
+const { getDogByName } = require("../controllers/dogsControllers/getDogByName");
+const { getBreedById } = require("../controllers/dogsControllers/getBreedById");
+const { createDog } = require("../controllers/dogsControllers/createDog");
+
 // el handler es una función que se va a encargar de recibir la req, de unificar datos y devolver la respuesta
 // invoca al controller, porque el handler nunca interactúa con fuentes externas
-const getDogsHandler = (req, res) => {
+
+const getDogsHandler = async (req, res) => {
+  try {
+    const dogsFromApi = await getAllDogsFromApi();
+    const dogsFromDB = await getAllDogsFromDB();
+    const allDogs = [...dogsFromApi, ...dogsFromDB];
+    if (allDogs.length > 0) {
+      res.status(200).json(allDogs);
+    } else {
+      res.status(404).json({ error: "Dogs not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getDogByNameHandler = async (req, res) => {
   try {
     const { name } = req.query;
-    if (name) res.status(200).send(`Aquí está la raza ${name}`);
-    res.status(200).send("Aquí están todos los dogs");
-  } catch (error) {}
+    const response = await getDogByName(name);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
+  }
 };
 
-const getBreedByIdHandler = (req, res) => {
+const getBreedByIdHandler = async (req, res) => {
   try {
-    const { idBreed } = req.params;
-    res.status(200).send(`Raza por ID: ${idBreed}`);
-  } catch (error) {}
+    const { id } = req.params;
+    const source = isNaN(id) ? "db" : "api";
+    const response = await getBreedById(id, source);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const createDogHandler = (req, res) => {
+const createDogHandler = async (req, res) => {
   try {
-    const {
-      id,
+    const { name, height, weight, life_span, image_id, temperament } = req.body;
+    const newDog = await createDog(
       name,
       height,
-      height_unit,
       weight,
-      weight_unit,
       life_span,
       image_id,
-    } = req.body;
-    res.status(200).send(`Dog: ${name}`);
-  } catch (error) {}
+      temperament
+    );
+    res.status(200).json(newDog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
   getDogsHandler,
+  getDogByNameHandler,
   getBreedByIdHandler,
   createDogHandler,
 };
