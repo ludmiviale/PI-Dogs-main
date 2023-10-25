d8905a6ef1f88ce73311219a88fdf01d40c09d72
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-25 13:34:25 -0300

    updating models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -2,41 +2,60 @@ const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define("dog", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter the name",
+          },
+        },
+      },
+      height: {
+        type: DataTypes.DECIMAL(5, 2),
+        allowNull: false,
+        validate: {
+          noNegativeValue(value) {
+            if (value < 0) {
+              throw new Error("Height cannot be negative");
+            }
+          },
+        },
+      },
+      weight: {
+        type: DataTypes.DECIMAL(5, 2),
+        allowNull: false,
+        validate: {
+          noNegativeValue(value) {
+            if (value < 0) {
+              throw new Error("Weight cannot be negative");
+            }
+          },
+        },
+      },
+      life_span: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter the life span",
+          },
+        },
+      },
+      reference_image_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    height: {
-      type: DataTypes.DECIMAL(5, 2),
-      allowNull: false,
-    },
-    height_unit: {
-      type: DataTypes.ENUM("inches", "centimeters"),
-      allowNull: false,
-    },
-    weight: {
-      type: DataTypes.DECIMAL(5, 2),
-      allowNull: false,
-    },
-    weight_unit: {
-      type: DataTypes.ENUM("pounds", "kilograms"),
-      allowNull: false,
-    },
-    life_span: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image_id: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-  });
+    { timestamps: false }
+  );
 };
--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -3,17 +3,25 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("temperament", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
+  sequelize.define(
+    "temperament",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Please enter the name",
+          },
+        },
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-  });
+    { timestamps: false }
+  );
 };
