739b0409f18b239b02dc8a0525a85c0f37a6bbb3
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-25 16:25:09 -0300

    updating routes

--- api/src/routes/dogsRouter.js
+++ api/src/routes/dogsRouter.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const dogsRouter = Router();
 const {
   getDogsHandler,
-  getBreedByIdHandler,
   createDogHandler,
+  getBreedByIdHandler,
 } = require("../handlers/dogsHandler");
 
 // acá se van a ejecutar los métodos con la response especificada, dependiendo del endpoint al que intento acceder
 dogsRouter.get("/", getDogsHandler);
-dogsRouter.get("/:idBreed", getBreedByIdHandler);
+dogsRouter.get("/:id", getBreedByIdHandler);
 dogsRouter.post("/", createDogHandler);
 
 module.exports = dogsRouter;
