5e33292a3b489c26e057457410ee8d901841fdfd
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-27 20:10:35 -0300

    updating dogsHandler

--- api/src/handlers/dogsHandler.js
+++ api/src/handlers/dogsHandler.js
@@ -2,6 +2,7 @@ const { getAllDogs } = require("../controllers/dogsControllers/getAllDogs");
 const { getDogByName } = require("../controllers/dogsControllers/getDogByName");
 const { getBreedById } = require("../controllers/dogsControllers/getBreedById");
 const { createDog } = require("../controllers/dogsControllers/createDog");
+const { updateDog } = require("../controllers/dogsControllers/updateDog");
 
 // el handler es una función que se va a encargar de recibir la req, de unificar datos y devolver la respuesta
 // invoca al controller, porque el handler nunca interactúa con fuentes externas
@@ -29,9 +30,9 @@ const getBreedByIdHandler = async (req, res) => {
   const { id } = req.params;
   try {
     const source = isNaN(id) ? "db" : "api";
-    const response = await getBreedById(id, source);
-    if (response) {
-      res.status(200).json(response);
+    const dogByBreed = await getBreedById(id, source);
+    if (dogByBreed) {
+      res.status(200).json(dogByBreed);
     } else {
       res.status(404).json({ message: "Breed not found" });
     }
@@ -58,8 +59,26 @@ const createDogHandler = async (req, res) => {
   }
 };
 
+const updateDogHandler = async (req, res) => {
+  const { id } = req.params;
+  const { height, newHeight, weight, newWeight } = req.body;
+  try {
+    const updatedDog = await updateDog(
+      id,
+      height,
+      newHeight,
+      weight,
+      newWeight
+    );
+    res.status(200).json(updatedDog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getDogsHandler,
   getBreedByIdHandler,
   createDogHandler,
+  updateDogHandler,
 };
