471b50c6f384cf821f8c045e4210d611825effc1
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-30 12:02:21 -0300

    updating main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./redux/store.js";
+import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
