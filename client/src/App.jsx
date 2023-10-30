963b47f0bc97b1d3a5f96a1fb7788256ec0180af
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-30 12:02:39 -0300

    updating app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Landing from "./views/landing/Landing";
+import Home from "./views/home/Home";
+import Detail from "./views/detail/Detail";
+import Create from "./views/create/Create";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/home/:id" element={<Detail />} />
+
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
 }
