fdd927276a79000f92dfc4069218c59ca463bc37
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-10-30 16:27:15 -0300

    [ADD] Home

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,12 +1,38 @@
-import Navbar from "../../components/navbar/Navbar";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+
+import { getAllDogs, getDogByName } from "../../redux/actions";
+
+import Nav from "../../components/nav/Nav";
 import Cards from "../../components/cards/Cards";
 
 function Home() {
+  const dispatch = useDispatch();
+  const allDogs = useSelector((state) => state.allDogs);
+
+  //Filtro con el backend
+  const [searchString, setSearchString] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSearchString(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(getDogByName(searchString));
+  };
+
+  useEffect(() => {
+    dispatch(getAllDogs());
+  }, [dispatch]);
+
   return (
     <div>
       <p> Esta es la Home Page </p>
-      <Navbar />
-      <Cards />
+      <Nav handleChange={handleChange} handleSubmit={handleSubmit} />
+      <Cards allDogs={allDogs} />
     </div>
   );
 }
