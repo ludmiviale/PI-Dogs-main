8cfb82708c8e5ceb0c28cc8029c444d57d20d627
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-01 09:45:15 -0300

    [ADD] Landing

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -1,9 +1,12 @@
-function Landing() {
+import { Link } from "react-router-dom";
+
+const Landing = () => {
   return (
     <div>
       <p> Esta es la Landing </p>
+      <Link to="/home">Home</Link>
     </div>
   );
-}
+};
 
 export default Landing;
