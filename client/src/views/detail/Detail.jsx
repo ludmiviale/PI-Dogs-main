e7c5f30524157454680da50c80160756ca04bfdb
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-01 09:45:32 -0300

    [ADD] Detail

--- client/src/views/detail/Detail.jsx
+++ client/src/views/detail/Detail.jsx
@@ -1,9 +1,34 @@
-function Detail() {
+import { useParams, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getDogById, dogsCleaner } from "../../redux/actions";
+
+const Detail = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const dogDetail = useSelector((state) => state.dogDetail);
+
+  useEffect(() => {
+    dispatch(getDogById(id));
+    return () => {
+      dispatch(dogsCleaner());
+    };
+  }, [id]);
+
   return (
     <div>
-      <p> Este es el Detail </p>
+      <Link to="/home">Home</Link>
+
+      <h2>{dogDetail.name}</h2>
+      <p>{dogDetail.weight}</p>
+      <p>{dogDetail.height}</p>
+      <p>{dogDetail.life_span}</p>
+      <p>{dogDetail.temperament}</p>
+      <img src={dogDetail.reference_image_id} alt="dog photo" />
+      <p>{dogDetail.id}</p>
     </div>
   );
-}
+};
 
 export default Detail;
