e52e7442445583a5d42f6c0cbbc4f544c31e1714
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-02 23:10:01 -0300

    [UPDATE] adding pagination

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -2,40 +2,58 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { dogsCleaner, getAllDogs, getDogByName } from "../../redux/actions";
+import { dogsCleaner, getDogs } from "../../redux/actions";
 
 import Nav from "../../components/nav/Nav";
 import Cards from "../../components/cards/Cards";
+import Pagination from "../../components/pagination/Pagination";
 
 const Home = () => {
   const dispatch = useDispatch();
   const allDogs = useSelector((state) => state.allDogs);
-  let [searchParams, setSearchParams] = useSearchParams();
 
+  const [searchParams, setSearchParams] = useSearchParams();
   const [searchString, setSearchString] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleChange = (event) => {
     event.preventDefault();
     setSearchString(event.target.value);
   };
 
+  const fetchDogs = (name) => {
+    dispatch(getDogs(name)).then(() => {
+      setCurrentPage(1);
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setSearchParams({ name: searchString });
-    dispatch(getDogByName(searchString));
+    fetchDogs(searchString);
+    setSearchString("");
   };
 
   useEffect(() => {
-    console.log(searchParams.get("name"));
     dispatch(dogsCleaner());
-    const queryName = searchParams.get("name");
-    if (queryName !== null) {
-      setSearchString(queryName);
-      dispatch(getDogByName(queryName));
-    } else {
-      dispatch(getAllDogs());
+    let queryName = searchParams.get("name");
+    if (queryName === null) {
+      queryName = "";
     }
+    setSearchString(queryName);
+    fetchDogs(queryName);
   }, []);
+
+  const totalDogs = allDogs?.length;
+  const dogsPerPage = 8;
+  const totalPages = Math.ceil(totalDogs / dogsPerPage);
+  const startIndex = (currentPage - 1) * dogsPerPage;
+  const endIndex = startIndex + dogsPerPage;
+  const dogsToDisplay = allDogs?.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div>
@@ -45,7 +63,12 @@ const Home = () => {
         handleSubmit={handleSubmit}
         searchString={searchString}
       />
-      <Cards allDogs={allDogs} />
+      <Pagination
+        onPageChange={handlePageChange}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
+      <Cards dogsToDisplay={dogsToDisplay} />
     </div>
   );
 };
