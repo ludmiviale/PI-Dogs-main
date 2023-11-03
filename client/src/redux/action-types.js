f650112c8ec31de2e5fb95d097029dfb2a3b83ce
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-02 23:06:05 -0300

    [UPDATE] actions-types

--- client/src/redux/action-types.js
+++ client/src/redux/action-types.js
@@ -1,4 +1,3 @@
-export const GET_ALL_DOGS = "GET_ALL_DOGS";
-export const GET_DOG_BY_NAME = "GET_DOG_BY_NAME";
+export const GET_DOGS = "GET_DOGS";
 export const GET_DOG_BY_ID = "GET_DOG_BY_ID";
 export const DOGS_CLEANER = "DOGS_CLEANER";
