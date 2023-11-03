52b848d35b3472750c376c644ecf47a948f82ec6
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-03 20:03:28 -0300

    updating controllers

--- api/src/controllers/temperamentControllers/getAllTemperaments.js
+++ api/src/controllers/temperamentControllers/getAllTemperaments.js
@@ -6,29 +6,33 @@ const { Temperament } = require("../../db");
 const getAllTemperaments = async (req, res) => {
   const { data } = await axios(`${URL}?api_key=${API_KEY}`);
 
-  const temperaments = data
-    .filter((dog) => dog.temperament)
-    .map((dog) => {
-      return {
-        name: dog.temperament,
-      };
-    });
+  const temperamentsDB = await Temperament.findAll();
 
-  const allTemperaments = [
-    ...new Set(
-      temperaments
-        .map((dog) => dog.name)
-        .join(", ")
-        .split(", ")
-        .sort()
-        .filter(Boolean)
-    ),
-  ];
+  if (!temperamentsDB.length) {
+    const temperaments = data
+      .filter((dog) => dog.temperament)
+      .map((dog) => {
+        return {
+          name: dog.temperament,
+        };
+      });
+    const allTemperaments = [
+      ...new Set(
+        temperaments
+          .map((dog) => dog.name)
+          .join(", ")
+          .split(", ")
+          .sort()
+          .filter(Boolean)
+      ),
+    ];
 
-  for (const temperament of allTemperaments) {
-    await Temperament.create({ name: temperament });
+    for (const temperament of allTemperaments) {
+      await Temperament.create({ name: temperament });
+    }
+    return allTemperaments;
   }
-  return allTemperaments;
+  return temperamentsDB;
 };
 
 module.exports = {
