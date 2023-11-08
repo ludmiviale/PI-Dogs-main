497ea82a6d975b83d72a68b9afe296fc239eb4f3
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-08 00:55:34 -0300

    updating index.js

--- api/index.js
+++ api/index.js
@@ -1,32 +1,12 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const {
   getAllTemperaments,
 } = require("./src/controllers/temperamentControllers/getAllTemperaments.js");
 const { conn } = require("./src/db.js");
 
-// cuando levanto mi servidor, quiero que se genere la vía de comunicación
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   getAllTemperaments();
   server.listen(3001, () => {
-    console.log("%s listening at 3001");
+    console.log("listening at 3001");
   });
 });
