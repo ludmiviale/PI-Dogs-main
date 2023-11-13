b510bda394ca7951243ac07692f917a6d03316d0
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-13 11:05:02 -0300

    [UPDATE] adding deleteDog

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteDog } from "../../redux/actions";
 import "./card.css";
 
 const Card = ({ dog }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteDog(id));
+  };
+
   const { id, name, weight, temperament, reference_image_id } = dog;
   return (
     <div className="card">
@@ -11,6 +19,11 @@ const Card = ({ dog }) => {
       <p>Weight: {weight}</p>
       <p>{temperament}</p>
       <img src={reference_image_id} alt="dog photo" />
+      {isNaN(id) ? (
+        <button onClick={() => handleDelete(id)}>Delete</button>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
