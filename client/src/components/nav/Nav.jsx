f7be629cd702bf44ddc7515be901464d64ac96fd
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-14 23:23:54 -0300

    [UPDATE] Nav

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,82 +1,97 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import Select from "../select/Select";
+import { filtersAndSorts } from "../../redux/actions";
 import "./nav.css";
 
-const Nav = ({
-  handleChange,
-  handleSubmit,
-  searchString,
-  handleFilterSource,
-  handleFilterTemperament,
-  handleSortAlphabetically,
-  handleSortByWeight,
-}) => {
-  const temperaments = useSelector((state) => state.temperaments);
+const Nav = ({ handlePageChange }) => {
+  const dispatch = useDispatch();
+  const { allDogs, temperaments, filters, sort } = useSelector(
+    (state) => state
+  );
+
+  const handleFilterChange = (event) => {
+    dispatch(
+      filtersAndSorts(
+        allDogs,
+        { ...filters, [event.target.name]: event.target.value },
+        sort
+      )
+    );
+    handlePageChange(1);
+  };
+
+  const handleSortChange = (event) => {
+    dispatch(filtersAndSorts(allDogs, filters, event.target.value));
+    handlePageChange(1);
+  };
 
   return (
     <nav className="nav">
-      <form onSubmit={(event) => handleSubmit(event)}>
-        <div className="search-container">
+      <div className="filters-container">
+        <div className="filter">
+          <label htmlFor="search">Filter by name:</label>
           <input
             autoComplete="off"
             id="search"
             type="search"
-            value={searchString}
-            placeholder="Search breeds"
-            onChange={(event) => handleChange(event)}
+            name="search"
+            value={filters?.search}
+            placeholder="Type for search"
+            onChange={(event) => handleFilterChange(event)}
           />
-          <button type="submit">Search</button>
+        </div>
+        <div className="filter">
+          <label htmlFor="source">Filter by source:</label>
+          <select
+            id="source"
+            name="source"
+            onChange={(event) => handleFilterChange(event)}
+            value={filters?.source}
+          >
+            <option value="all">All</option>
+            <option value="api">Api</option>
+            <option value="database">Database</option>
+          </select>
         </div>
 
-        <div className="filters-container">
-          <div className="filter">
-            <label htmlFor="source">Filter by source:</label>
-            <select id="source" name="source" onChange={handleFilterSource}>
-              <option value="all">All</option>
-              <option value="api">Api</option>
-              <option value="database">Database</option>
-            </select>
-          </div>
-
-          <div className="filter">
-            <label htmlFor="temperament">Filter by temperament:</label>
-            <Select
-              name="temperaments"
-              values={temperaments}
-              handleChange={handleFilterTemperament}
-              includeAll={true}
-            />
-          </div>
-
-          <div className="filter">
-            <label htmlFor="sortAlphabetically">Sort alphabetically:</label>
-            <select
-              id="sortAlphabetically"
-              name="sortAlphabetically"
-              onChange={handleSortAlphabetically}
-            >
-              <option value="ascending">A-Z</option>
-              <option value="descending">Z-A</option>
-            </select>
-          </div>
-
-          <div className="filter">
-            <label htmlFor="sortByWeight">Sort by weight:</label>
-            <select
-              id="sortByWeight"
-              name="sortByWeight"
-              onChange={handleSortByWeight}
-            >
-              <option value="lowerWeight">Lower weight</option>
-              <option value="greaterWeight">Greater weight</option>
-            </select>
-          </div>
+        <div className="filter">
+          <label htmlFor="temperament">Filter by temperament:</label>
+          <select
+            id="temperament"
+            name="temperament"
+            onChange={(event) => handleFilterChange(event)}
+            value={filters?.temperament}
+          >
+            <option value="all">All</option>
+            {temperaments.map((temperament) => (
+              <option key={temperament.id} value={temperament.name}>
+                {temperament.name}
+              </option>
+            ))}
+          </select>
         </div>
-        <Link to="/create">
-          <button id="create">ADD</button>
-        </Link>
-      </form>
+
+        <div className="filter">
+          <label htmlFor="sort">Sort:</label>
+          <select
+            id="sort"
+            name="sort"
+            onChange={(event) => handleSortChange(event)}
+            value={sort}
+          >
+            <option value="">None</option>
+            <option value="alphabeticalAsc">Alphabetically A-Z</option>
+            <option value="alphabeticalDesc">Alphabetically Z-A</option>
+            <option value="lowerWeight">Lower weight</option>
+            <option value="greaterWeight">Greater weight</option>
+          </select>
+        </div>
+        <div className="filter">
+          <Link to="/create">
+            <button id="create">ADD</button>
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
