ff61c390a591bf37b6657f617f3074ed1cde5810
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-14 23:22:28 -0300

    [UPDATE] Home

--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,13 +1,6 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getDogs,
-  filterBySource,
-  filterByTemperament,
-  sortAlphabetically,
-  sortByWeight,
-} from "../../redux/actions";
+import { getAllTemperaments, getDogs } from "../../redux/actions";
 import Nav from "../../components/nav/Nav";
 import Cards from "../../components/cards/Cards";
 import Pagination from "../../components/pagination/Pagination";
@@ -15,33 +8,12 @@ import "./home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { allDogs, allDogsCopy } = useSelector((state) => state);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchString, setSearchString] = useState("");
+  const { allDogs, allDogsCopy, filters, sort } = useSelector((state) => state);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const fetchDogs = (name) => {
-    dispatch(getDogs(name)).then(() => {
-      setCurrentPage(1);
-    });
-  };
-
-  const handleChange = (event) => {
-    setSearchString(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSearchParams({ name: searchString });
-    fetchDogs(searchString);
-    setSearchString("");
-  };
-
   useEffect(() => {
-    let queryName = searchParams.get("name");
-    queryName = queryName === null ? "" : queryName;
-    setSearchString(queryName);
+    dispatch(getDogs(filters, sort));
+    dispatch(getAllTemperaments());
   }, []);
 
   const totalDogs = allDogsCopy?.length;
@@ -55,39 +27,9 @@ const Home = () => {
     setCurrentPage(newPage);
   };
 
-  const handleFilterTemperament = (event) => {
-    dispatch(filterByTemperament(event.target.value));
-    setCurrentPage(1);
-    setSearchParams("");
-  };
-
-  const handleFilterSource = (event) => {
-    dispatch(filterBySource(event.target.value));
-    setCurrentPage(1);
-    setSearchParams("");
-  };
-
-  const handleSortAlphabetically = (event) => {
-    dispatch(sortAlphabetically(event.target.value));
-    setCurrentPage(1);
-  };
-
-  const handleSortByWeight = (event) => {
-    dispatch(sortByWeight(event.target.value));
-    setCurrentPage(1);
-  };
-
   return (
     <div>
-      <Nav
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        searchString={searchString}
-        handleFilterTemperament={handleFilterTemperament}
-        handleFilterSource={handleFilterSource}
-        handleSortAlphabetically={handleSortAlphabetically}
-        handleSortByWeight={handleSortByWeight}
-      />
+      <Nav handlePageChange={handlePageChange} />
       <Pagination
         onPageChange={handlePageChange}
         currentPage={currentPage}
