9e59e82ea9bbf885b5ee564ea81ec866a6e93411
Ludmila Grisel Viale <ludmilaviale@hotmail.com>
2023-11-14 23:21:47 -0300

    [UPDATE] App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import Landing from "./views/landing/Landing";
 import Home from "./views/home/Home";
 import Detail from "./views/detail/Detail";
 import Create from "./views/create/Create";
-import { getDogs, getAllTemperaments } from "./redux/actions";
 
 import "./App.css";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getDogs(""));
-    dispatch(getAllTemperaments());
-  }, []);
-
   return (
     <div className="App">
       <Routes>
